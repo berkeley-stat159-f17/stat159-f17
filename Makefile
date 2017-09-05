52c776b1ac76846bb271791c8e6426f3db953ae5
Fernando Perez <Fernando.Perez@berkeley.edu>
2017-09-05 01:00:49 -0700

    Add markdown support

--- Makefile
+++ Makefile
@@ -18,3 +18,10 @@ help:
 # "make mode" option.  $(O) is meant as a shortcut for $(SPHINXOPTS).
 %: Makefile
 	@$(SPHINXBUILD) -M $@ "$(SOURCEDIR)" "$(BUILDDIR)" $(SPHINXOPTS) $(O)
+
+github: html
+	# cp -a data $(BUILDDIR)/html/.
+	ghp-import $(BUILDDIR)/html/
+	# git push -u origin gh-pages
+	@echo
+	@echo "Published to Github"
--- conf.py
+++ conf.py
@@ -40,8 +40,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+#source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
@@ -77,6 +77,12 @@ pygments_style = 'sphinx'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
+
+
+# Use markdown
+source_parsers = {
+   '.md': 'recommonmark.parser.CommonMarkParser',
+}
 
 
 # -- Options for HTML output ----------------------------------------------
